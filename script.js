35bfc76c4c0dee49f2821579fd6acb526d88765d
kajaljagtap2005 <kajaljagtapofficial@gmail.com>
2026-03-02 19:05:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// EmailJS init 
+emailjs.init("YOUR_PUBLIC_KEY");  
+
+const form = document.getElementById('bookingForm');
+const successModal = document.getElementById('successModal');
+
+const checkin = document.getElementById('checkin');
+const checkout = document.getElementById('checkout');
+const roomType = document.getElementById('roomType');
+const adults = document.getElementById('adults');
+const priceBox = document.getElementById('priceBox');
+const totalPrice = document.getElementById('totalPrice');
+const nightsText = document.getElementById('nights');
 
